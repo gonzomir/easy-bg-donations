2eb3c9f3947d5401862f64886be0dc83d8614e61
Milen Petrinski <gonzo@greatgonzo.net>
2023-08-01 11:13:09 +0300

    Add POT file and Bulgarian translation, load translations in PHP.

--- inc/namespace.php
+++ inc/namespace.php
@@ -8,4 +8,14 @@ namespace Gonzo\Easy_BG_Donations;
  * @return void
  */
 function bootstrap() {
+	add_action( 'plugins_loaded', __NAMESPACE__ . '\\load_textdomain' );
+}
+
+/**
+ * Load translations.
+ *
+ * @return void
+ */
+function load_textdomain() {
+	load_plugin_textdomain( 'ebd', false, basename( dirname ( __DIR__ ) ) . '/languages/' );
 }
